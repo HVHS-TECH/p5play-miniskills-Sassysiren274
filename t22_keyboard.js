9c62e63271444d0bf9519ffdb7fb25ba5ccd2d28
Sassysiren274 <25471jf@hvhs.school.nz>
2026-02-25 14:30:49 +1300

    bb

--- t22_keyboard.js
+++ t22_keyboard.js
@@ -12,40 +12,33 @@ function setup() {
 //canvas
 	cnv = new Canvas(1250,650);
 	//world.gravity.y = 10;
-	
+  // Create your sprite
   Square = new Sprite(300,300,200,200,);
   Square.color = 'Black';
-  Square.rotationSpeed = 2;
+  //Square.rotationSpeed = 2;
   //Square.vel.x = 2;
- wallLH  = new Sprite(0, height/2, 8, height, 'k');
-  wallLH.color = 'black';
-  wallRH  = new Sprite(width, height/2, 8, height, 'k');
-  wallRH.color = 'black';
-  wallTop = new Sprite(width/2, 0, width, 8, 'k');
-  wallTop.color = 'black';
-  wallBot = new Sprite(width/2, height, width, 8, 'k');
-  wallBot.color = 'black';
 }
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	background('navy');
-if (!mouse.presses()) {
-    Square.moveTowards(mouseX, mouseY, 1); // smooth movement
-  }
 
-  // If mouse is pressed, move to a fixed position at speed 5
-  if (mouse.presses()) {
-    Square.moveTo(200, 150, 50);
-  }
+  background('lightgrey');
+
   if (kb.pressing('left')) {
-  // Set sprite's velocity to the left
+Square.vel.x = -5;
 }
+
 else if (kb.pressing ('right')) {
+Square.vel.x = 5;
 };
+
 if (kb.released('left')) {
-// Set sprite's velocity to zero
+  Square.vel.x = 0;
+}
+
+if (kb.released('right')) {
+  Square.vel.x = -0;
 }
 }
 /*******************************************************/
